8ea9e0d12ba53657466093fdebb754f509fb43ea
unknown <gustavocviana99@gmail.com>
2026-02-02 13:38:29 -0300

    add dominio render

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,7 @@ app.set('trust proxy', 1);
 const allowedOrigins = [
   'https://gustavocampos1999.github.io', 
   'http://127.0.0.1:5500',
+  'https://sisdecor.com.br/',
   'http://localhost:5500'
 ];
 
